12be1105c3c648f6072118b4c5237f732dc00508
c6y <k@eboy.com>
2018-11-10 17:29:24 -0800

    initial dowload of modified new theme

---
+++ assets/script-toggle_01k.js
@@ -0,0 +1,10 @@
+function toggle( targetId ){ 
+  if (document.getElementById){ 
+        target = document.getElementById( targetId ); 
+           if (target.style.display == "none"){ 
+              target.style.display = ""; 
+           } else { 
+              target.style.display = "none"; 
+           } 
+     } 
+}
